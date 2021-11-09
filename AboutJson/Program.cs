cf5a56d65a099752ae155b80cbd85ccbf83309f2
junkichi424 <akiyama-junki@outlook.jp>
2021-11-09 23:28:51 +0900

    newtonsoftを使ったjson作成を記述。

---
+++ AboutJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+
+MyNewtonsoft.CreateJson();
+
+
+public class MyNewtonsoft 
+{
+    public static void CreateJson()
+    {
+        // オブジェクトを作る
+        var oneday = new WeatherForecast();
+        oneday.Date = DateTime.Now;
+        oneday.Summary = "だいたい晴れかも";
+        oneday.MustItems = new string[] {"傘","てるてる坊主" };
+        oneday.TemperatureCelsius = 25;
+
+        var temperatureranges = new Temperatureranges()
+        {
+            Hot = new Hot() {High = 30,Low = 25},
+            Cold = new Cold {High = 15,Low = 10 }            
+        };
+        oneday.TemperatureRanges = temperatureranges;
+
+        // シリアライズする 第2引数インデントをつける
+        var json = JsonConvert.SerializeObject(oneday, Formatting.Indented);
+
+        Console.WriteLine(json);
+     }
+}
+
+
---
+++ AboutJson/WeatherForecast.cs
@@ -0,0 +1,27 @@
+﻿
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+    public int TemperatureCelsius { get; set; }
+    public string Summary { get; set; }
+    public Temperatureranges TemperatureRanges { get; set; }
+    public string[] MustItems { get; set; }
+}
+
+public class Temperatureranges
+{
+    public Cold Cold { get; set; }
+    public Hot Hot { get; set; }
+}
+
+public class Cold
+{
+    public int High { get; set; }
+    public int Low { get; set; }
+}
+
+public class Hot
+{
+    public int High { get; set; }
+    public int Low { get; set; }
+}
